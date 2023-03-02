42e87f1ff6167a814643b438992b3e44d8cd5d43
kerim-agayev <120748518+kerim-agayev@users.noreply.github.com>
2023-03-02 08:13:59 -0700

    project nextjs
    next js quruldu

---
+++ food_ordering/pages/_document.js
@@ -0,0 +1,26 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+{/*font - awesome -dancing fonts */}
+      <link 
+        rel="preconnect" 
+        href="https://fonts.googleapis.com"/>
+        <link 
+        rel="preconnect" 
+        href="https://fonts.gstatic.com" 
+        crossOrigin='true'/>
+        <link 
+        href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;700&display=swap" 
+        rel="stylesheet"/>
+        {/* font - awesome -dancing fonts*/}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ food_ordering/pages/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+        <div className='font-dancing text-red-600 text-center text-3xl'>Feane</div>
+   
+    </>
+
+  )
+}
+
+export default Home
