2dfd6d265d7c0a88737f434664d59a2cc85a788d
kerim-agayev <120748518+kerim-agayev@users.noreply.github.com>
2023-03-05 05:21:33 -0700

    Menu wrapper
    Menu wrapper

--- food_ordering/pages/home/index.js
+++ food_ordering/pages/home/index.js
@@ -1,11 +1,14 @@
 import Campaigns from '@/components/Campaigns';
+import MenuWrapper from '@/components/MenuWrapper';
 import React from 'react'
 import Carousel from "../../components/Carousel";
+
 function Home() {
   return (
     <div>
     <Carousel />
     <Campaigns />
+    <MenuWrapper />
   </div>
   );
  
