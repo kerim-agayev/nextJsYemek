0338ccde5dcab536cf1dac88dc495b1b9da929ee
kerim-agayev <120748518+kerim-agayev@users.noreply.github.com>
2023-03-07 06:14:32 -0700

    customers component
    customers component

--- food_ordering/pages/home/index.js
+++ food_ordering/pages/home/index.js
@@ -4,16 +4,21 @@ import MenuWrapper from '@/components/MenuWrapper';
 import React from 'react'
 import Carousel from "../../components/Carousel";
 import Reservation from '../../components/Reservation'
+import Customers from '../../components/Customers'
 function Home() {
   return (
-    <div>
+    // <React.Fragment>
+
+
+    <>
     <Carousel />
     <Campaigns />
     <MenuWrapper />
     <About />
     <Reservation />
-    
-  </div>
+    <Customers />
+  </>
+      // </React.Fragment>
   );
  
 
