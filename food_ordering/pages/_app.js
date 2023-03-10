edbe3ba5d9d8fd3b91bd3cdddbfccb028fda2e9e
kerim-agayev <120748518+kerim-agayev@users.noreply.github.com>
2023-03-10 07:12:32 -0700

    redux-toolkit store
    redux-toolkit store

--- food_ordering/pages/_app.js
+++ food_ordering/pages/_app.js
@@ -3,14 +3,17 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Layout from "@/layout/Layout";
 import "../styles/globals.css";
-
+import { Provider } from "react-redux";
+import store from "../redux/store";
 
 
  function App({ Component, pageProps }) {
   return (
+    <Provider store={store}>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+  </Provider>
   );
 }
 export default App;
--- food_ordering/pages/product/index.js
+++ food_ordering/pages/product/index.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { useState } from "react";
 import Title from "@/components/ui/Title";
+import { addProduct } from "../../redux/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
 const itemsExtra = [
   {
     id: 1,
@@ -18,13 +20,30 @@ const itemsExtra = [
     price: 3,
   },
 ];
+const foodItems = [
+  {
+    id: 1,
+    name: "Pizza 1",
+    price: 10,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda fugit corporis ex laboriosam tenetur at ad aspernatur",
+    extraOptions: [
+      {
+        id: 1,
+        name: "Extra 1",
+        price: 1,
+      },
+    ],
+  },
+];
 const Index = () => {
   const [prices, setPrices] = useState([10, 20, 30]);
   const [price, setPrice] = useState(prices[0]);
   const [size, setSize] = useState(0);
   const [extraItems, setExtraItems] = useState(itemsExtra);
   const [extras, setExtras] = useState([]);
+  const cart = useSelector((state) => state.cart);
 
+  const dispatch = useDispatch();
   
   const handleSize = (sizeIndex) => {
     const difference = prices[sizeIndex] - prices[size];
@@ -41,18 +60,23 @@ const Index = () => {
 
     if (checked) {
       changePrice(item.price);
-      // setExtras([...extras, item]);
+      setExtras([...extras, item]);
     } else {
       changePrice(-item.price);
-      // setExtras(extras.filter((extra) => extra.id !== item.id));
+       setExtras(extras.filter((extra) => extra.id !== item.id));
     }
   };
   // console.log(extras);
+  const handleClick = () => {
+    dispatch(addProduct({ ...foodItems[0], extras, price, quantity: 1 }));
+  };
+
+  console.log(cart);
 
   return (
      <div className="flex items-center md:h-[calc(100vh_-_88px)] gap-5 py-20 flex-wrap ">
       <div className="relative md:flex-1 md:w-[80%] md:h-[80%] w-36 h-36 mx-auto">
-        <Image src="/images/f1.png" alt="" layout="fill" objectFit="contain" />
+        <Image src="/images/f1.png" alt="" fill objectFit="contain" />
       </div>
       <div className="md:flex-1 md:text-start text-center">
         <Title addClass="text-6xl">Good Pizza</Title>
@@ -108,7 +132,9 @@ const Index = () => {
             </label>
           ))}
         </div>
-        <button className="btn-primary">Add to Cart</button>
+        <button className="btn-primary" onClick={handleClick}>
+          Add to Cart
+        </button>
       </div>
     </div>
   );
